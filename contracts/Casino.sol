b81208fc0fb2f8c4438986f3e07e367fac84abab
Aivaras Šimulis <aivaras.simulis@outlook.com>
2018-01-15 22:23:33 +0200

    Setup project

---
+++ contracts/Casino.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.4.11; // 17
+
+contract Casino {
+  address owner;
+
+  uint minimumBet;
+  uint totalBet;
+  uint numberOfBets;
+  uint maxAmountOfBets = 100;
+  address[] players;
+
+  struct Player {
+    uint amountBet;
+    uint numberSelected;
+  }
+
+  mapping(address => Player) playerInfo;
+
+  function Casino(uint _minimumBet) {
+    owner = msg.sender;
+    if (_minimumBet != 0) {
+      minimumBet = _minimumBet;
+    }
+  }
+
+  function kill() {
+    if (msg.sender == owner) {
+      selfdestruct(owner);
+    }
+  }
+
+  // To bet for a number between 1 and 10 both inclusive
+  function bet(uint number) payable {
+    assert(checkPlayerExists(msg.sender) == false);
+    assert(number >= 1 && number <= 10);
+    assert(msg.value >= minimumBet);
+
+    playerInfo[msg.sender].amountBet = msg.value;
+    playerInfo[msg.sender].numberSelected = number;
+    numberOfBets += 1;
+    players.push(msg.sender);
+    totalBet += msg.value;
+
+    if (numberOfBets >= maxAmountOfBets) {
+      generateNumberWinner();
+    }
+  }
+
+  function checkPlayerExists(address player) returns(bool) {
+    for(uint i = 0; i < players.length; i++) {
+      if (players[i] == player) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function generateNumberWinner() {
+    uint numberGenerated = block.number % 10 + 1 // This isn't secure
+    distributePrizes(numberGenerated);
+  }
+}
+
